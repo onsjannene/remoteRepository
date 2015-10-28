afa093ec0808220629d8844a06828079d2925341
ons <ons@ons-TOSH>
2015-10-28 15:50:39 +0100

    adding divide methode
    Signed-off-by: ons <ons@ons-TOSH>

--- calculate/src/main/java/edu/esprit/calculate/Calculator.java
+++ calculate/src/main/java/edu/esprit/calculate/Calculator.java
@@ -14,5 +14,13 @@ public class Calculator {
 		return result;
 		
 	}
+	
+	
+	public Double  divide(Double d1, Double d2){
+		
+		if(d2==0)
+			throw new IllegalArgumentException("cant divide by 0!!!");
+		return (d1/d2);
+	}
 
 }
